8260d28b8c56e754d2e1dd54ea2659ac772aaa03
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-05-16 13:49:26 +0200

    Test Azure 2

--- Secrets.cs
+++ Secrets.cs
@@ -1,11 +1,17 @@
 ﻿using COPVETFinances.Migrations;
+using Microsoft.AspNetCore.Hosting.Server;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace COPVETFinances
 {
+    //public class Secrets
+    //{
+    //    public static readonly string conn = "Data Source=copvetfinancesdbserver.database.windows.net;Initial Catalog=COPVETFinances_db;User ID=Mbah;Password=Elvisco1989;Connect Timeout=60;Trust Server Certificate=True";
+    //}
+
     public class Secrets
     {
-        public static readonly string conn = "Data Source=copvetfinancesdbserver.database.windows.net;Initial Catalog=COPVETFinances_db;User ID=Mbah;Password=Elvisco1989;Connect Timeout=60;Trust Server Certificate=True";
+        public static readonly string conn = "Server = tcp:copvetfinancesdbserver.database.windows.net,1433;Initial Catalog = COPVETFinances_db; Persist Security Info=False;User ID = Mbah; Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30";
     }
 
 
